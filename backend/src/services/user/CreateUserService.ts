fe89ac02253bfb44e9fdfc92425bf9274ba69bc5
Bianca Balabenute <balabenute.bianca@gmail.com>
2023-10-14 11:43:32 -0300

    cadastrando usuario

---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,43 @@
+import prismaClient from "../../prisma";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+
+    if (!email) {
+      throw new Error("Email incorreto")
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (userAlreadyExists) {
+      throw new Error("Email já cadastrado")
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    })
+
+    return user;
+  }
+}
+
+export { CreateUserService }
