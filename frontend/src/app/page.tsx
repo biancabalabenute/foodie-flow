2e3c85c5256efe63b9ef74952cdb6b8a71f6523f
Ana Guassu <annaguassu@gmail.com>
2023-10-19 21:38:15 -0300

    Criando tela login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,30 @@
 import RootLayout from './layout'
+import Image from 'next/image'
+import styles from './page.module.scss'
+
+import logoImg from '../../../frontend/public/android-chrome-192x192.png'
+
+import { Input } from '../component/ui/input/index'
 
 export default function MinhaPagina() {
   return (
     <RootLayout title="FoodieFlow - Login">
-      <h1>
-        FoodieFlow :)
-      </h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="FoodieFlow Pedidos" />
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder='Digite seu email'
+              type='text'
+            />
+            <Input
+              placeholder='Digite sua senha'
+              type='password'
+            />
+          </form>
+        </div>
+
+      </div>
     </RootLayout>
   )
 }
