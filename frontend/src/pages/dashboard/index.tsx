9a9bf154cf573e2f470a770dcb232aabc8607455
Ana Guassu <annaguassu@gmail.com>
2023-11-03 01:44:46 -0300

    Cadastrando dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,26 +1,45 @@
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from "next/head"
+import styles from './styles.module.scss'
 
 import { Header } from "../../components/Header"
+import { FiRefreshCcw } from "react-icons/fi"
 
-export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    return (
         <>
-        <Head>
-            <title>Painel - FoodieFlow</title>
-        </Head>
-        <div>
-            <Header/>
-            <h1>Painel</h1>
-        </div>
+            <Head>
+                <title>Painel - FoodieFlow</title>
+            </Head>
+            <div>
+                <Header />
+                <main className={styles.container}>
+                    <div className={styles.containerHeader}>
+                        <h1>Últimos Pedidos</h1>
+                        <button>
+                            <FiRefreshCcw size={25} color="#3fffa3" />
+                        </button>
+                    </div>
+
+                    <article className={styles.listOrders}>
+                        <section className={styles.orderItem}>
+                            <button>
+                                <div className={styles.tag}></div>
+                                <span>Mesa 30</span>
+                            </button>
+                        </section>
+                    </article>
+
+                </main>
+            </div>
         </>
     )
 }
 
-export const getServerSideProps = canSSRAuth(async(ctx) => {
+export const getServerSideProps = canSSRAuth(async (ctx) => {
 
-    return{
-        props:{
+    return {
+        props: {
 
         }
     }
